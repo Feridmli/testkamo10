af21e2097c21d6f10b7b53333cb39ce99ba25066
Feridmli <162040986+Feridmli@users.noreply.github.com>
2025-11-30 21:45:16 +0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import { Seaport } from "@opensea/seaport-js";
 // 1. KONFIQURASIYA VƏ SABİTLƏR
 // ==========================================
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://azekamo100.onrender.com";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://testkamo10.onrender.com";
 const NFT_CONTRACT_ADDRESS = import.meta.env.VITE_NFT_CONTRACT || "0x54a88333F6e7540eA982261301309048aC431eD5";
 
 // Seaport 1.5 Canonical Address
